14ca38a9b62fae48dc8b0c6987de8740488b9705
Joakim Bertils <metallica317@spray.se>
2017-08-18 08:34:05 +0200

    Tog bort deps

--- deps/include/errno.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LIBK_ERRNO_H
-#define _LIBK_ERRNO_H
-
-#define EDOM -1
-#define ERANGE -2
-
-#endif
--- deps/include/memory.h
+++
@@ -1,8 +0,0 @@
-/**
- * @file    memory.h
- * @author  Joakim Bertils
- * @date    2017-07-25
- * @brief   LIBK memory.h Standard Header
- */
-
-#include <string.h>
